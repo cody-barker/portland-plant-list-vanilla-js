fff2d1063f1f86233c428359dea2207df197f077
Cody <codybarker.or@gmail.com>
2023-01-30 10:39:20 -0800

    added comments to sort feature

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-
-//Potential Application features
+//PHASE-1-PROJECT
+//
+//Application features
 // x 1. Sort columns alphanumerically by clicking on the column header
 // x 2. Highlight plants when clicking anywhere on their row
-//   3. Filter the list based on clicked plants to create a custom list
-// x 4. A form for adding new plants to the database
-//   5. Click on a plant img thumbnail to expand it
-//   6. Filter the list based on clicked plants to create a custom list
+// x 3. A form for adding new plants to the database
 
 //Project Requirements
 // x  3 Distinct Event Listener Types 
 // x    1) Click - highlight row, sort columns alphanumerically
-// x    2) DOMContentLoaded - render plants to the list, add the submission form, add the column sorting feature
-// x    3) Submit - form
-//      4) Click - expand image
+// x    2) DOMContentLoaded - render plants to the list, listener for form, add the column sorting feature
+// x    3) Submit - plant submission form
 //
 // x  Must return at least 5 objects from the server with >=3 attributes per object
 // x  Request/Response cycle should be async, using JSON
@@ -27,8 +24,13 @@
 //Stretch goals
 // x  Use json-server in your project to persist your app's interactivity
 
-//After the html loads, populate the full Plant List
-//pass an anonymous function instead of listing all 3
+
+
+
+//Run JS after domcontent loaded
+document.addEventListener("DOMContentLoaded", initialize)
+
+//for DOMContentLoaded
 function initialize () {
     initList()
     formAdd()
@@ -44,55 +46,6 @@ function initList() {
         })
 }
 
-//Add a submit event listener to the form
-function formAdd() {
-    const form = document.querySelector('form')
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let plantObj = {
-            binomialName: e.target.binomialNameInput.value,
-            commonName: e.target.commonNameInput.value,
-            type: e.target.typeInput.value,
-            height: e.target.heightInput.value,
-            lightRequirement: e.target.lightRequirementInput.value,
-            moistureRequirement: e.target.moistureRequirementInput.value
-        }
-        addPlant(plantObj)
-    })
-}
-
-//SORT ALPHANUMERICALLY
-//1. Assign event listeners to all TH tags
-//src = https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
-//Consider making this feature part of a separate branch before the review
-//Learn git branching (game)
-function columnSort() {
-    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-
-    const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
-
-// do the work...
-    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-    const table = th.closest('table');
-    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
-        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-        .forEach(tr => table.appendChild(tr) );
-})));
-}
-
-//Clicking anywhere in a row highlights it
-function addHighlightListener(newRow) {
-    newRow.addEventListener('click', (e) => {
-        e.target.parentNode.style.backgroundColor = (e.target.parentNode.style.backgroundColor === 'rgb(4, 170, 109)' ) ? ('transparent'):('rgb(4, 170, 109)');
-        console.log(e.target.parentNode.style.backgroundColor)
-    })
-}
-
-
-//consider refactoring with helper functions
-//the rendering might be done by a function that takes a plantObj as an argument, and creates all the td cells with its properties
 function renderOnePlant(plantObj) {
     //Create a new row in the table for each plant
     const newRow = document.createElement('tr')
@@ -111,11 +64,36 @@ function renderOnePlant(plantObj) {
     tdHeight.innerText = plantObj.height
     tdLightRequirement.innerText = plantObj.lightRequirement
     tdMoistureRequirement.innerText = plantObj.moistureRequirement
-    //Add highlighting event listener
-    addHighlightListener(newRow);
     //Append the new plant to table#mainList
     newRow.append(tdBinomialName, tdCommonName, tdType, tdHeight, tdLightRequirement, tdMoistureRequirement)
     document.querySelector('#mainList').append(newRow)
+    //Add highlighting event listener to each row
+    addHighlightListener(newRow);
+}
+
+//Clicking anywhere in a row highlights it
+function addHighlightListener(newRow) {
+    newRow.addEventListener('click', (e) => {
+        e.target.parentNode.style.backgroundColor = 
+(e.target.parentNode.style.backgroundColor === 'rgb(4, 170, 109)' ) ? ('transparent'):('rgb(4, 170, 109)');
+    })
+}
+
+//Add a 'submit' event listener to the form
+function formAdd() {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const plantObj = {
+            binomialName: e.target.binomialNameInput.value,
+            commonName: e.target.commonNameInput.value,
+            type: e.target.typeInput.value,
+            height: e.target.heightInput.value,
+            lightRequirement: e.target.lightRequirementInput.value,
+            moistureRequirement: e.target.moistureRequirementInput.value
+        }
+        addPlant(plantObj)
+    })
 }
 
 //Persist the form submission to the database and render it to the DOM
@@ -129,8 +107,30 @@ function addPlant(plantObj) {
     })
         .then(resp => resp.json())
         .then(() => renderOnePlant(plantObj))
+        .catch(error => console.log(error))
 }
 
+//SORT ALPHANUMERICALLY
+//src = https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+//Consider making this feature part of a separate branch before the review
+//Learn git branching (game)
+// //Quick explanation
+// 1. add a click event to all header (th) cells...
+// 2. for the current table, find all rows (except the first)...
+// 3. sort the rows, based on the value of the clicked column...
+// 4. insert the rows back into the table, in the new order.
+function columnSort() {
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-//Run JS after domcontent loaded
-document.addEventListener("DOMContentLoaded", initialize)
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+// do the work...
+    document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => table.appendChild(tr) );
+})));
+}
